33f2c9eb057c8484465d64eef5badb231aae2a11
Lê Tấn Bảo Bảo <110554959+thomaswilsondev@users.noreply.github.com>
2023-06-07 01:46:24 +0700

    fix media mobile page top

--- src/pages/PageTop/PageTop.component.jsx
+++ src/pages/PageTop/PageTop.component.jsx
@@ -3,11 +3,7 @@ import Navigation from "./Navigation/Navigation.component";
 const PageTop = () => {
   return (
     <section
-<<<<<<< HEAD
-      className="PageTop border-b-2 border-[#7B4AE2] px-[10rem] pt-[36px]"
-=======
-      className=" h-screen  border-b-2 border-[#7B4AE2] px-[10.9rem] pt-[36px]"
->>>>>>> origin/main
+      className="PageTop w-screen h-screen border-b-2 border-[#7B4AE2] px-[10rem] pt-[36px]"
       id="home"
       data-aos="fade-up"
       data-aos-delay="100"
